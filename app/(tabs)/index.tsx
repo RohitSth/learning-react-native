c449ef0737c0c031c1bed6052bbd1d77c25994a2
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-05-14 09:27:30 +0545

    added text core component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,15 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "skyblue" }}>
-      <View style={{ width: 200, height: 200, backgroundColor: "plum" }}></View>
+    <View style={{ flex: 1, backgroundColor: "skyblue", padding: 60 }}>
+      {/* <View style={{ width: 200, height: 200, backgroundColor: "plum" }}></View>
       <View
         style={{ width: 200, height: 200, backgroundColor: "lightgreen" }}
-      ></View>
+      ></View> */}
+      <Text>
+        <Text style={{ color: "white" }}>Hello</Text> World!!
+      </Text>
     </View>
   );
 }
