104405b5bf30c8ce0bb4b56c831b769d48af6d44
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-05-14 11:10:45 +0545

    pressable core component learned

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,6 +5,7 @@ import {
   ImageBackground,
   ScrollView,
   Button,
+  Pressable,
 } from "react-native";
 
 const logoImg = require("../../assets/images/adaptive-icon.png");
@@ -13,23 +14,34 @@ export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "skyblue", padding: 60 }}>
       <ScrollView>
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-          repudiandae aperiam repellat, amet, molestiae ullam c odit libero
-          sequi.umque, debitis perferendis veniam tempora error corrupti non
-          impedit earum deleniti sed saepe iure ea!Lorem Lorem, ipsum dolor sit
-          amet consectetur adipisicing elit. Adipisci ab culpa corrupti modi,
-          consequuntur fugit delectus quas rerum provident magni dolor a
-          excepturi repellendus nam quia! Doloribus Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Veritatis repudiandae aperiam repellat,
-          amet, molestiae ullam c odit libero sequi.umque, debitis perferendis
-          veniam tempora error corrupti non impedit earum deleniti sed saepe
-          iure ea!Lorem Lorem, ipsum dolor sit amet consectetur adipisicing
-          elit. Adipisci ab culpa corrupti modi, consequuntur fugit delectus
-          quas rerum provident magni dolor a excepturi repellendus nam quia!
-          Doloribus
-        </Text>
+        <Pressable
+          onPressIn={() => console.log("Image Pressed In")}
+          onLongPress={() => console.log("Image Hold")}
+          onPressOut={() => console.log("Pressed Out")}
+        >
+          <Image
+            source={{ uri: "https://picsum.photos/seed/picsum/200/300" }}
+            style={{ width: 300, height: 300 }}
+          />
+        </Pressable>
+        <Pressable onPress={() => console.log("Text Pressed")}>
+          <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            repudiandae aperiam repellat, amet, molestiae ullam c odit libero
+            sequi.umque, debitis perferendis veniam tempora error corrupti non
+            impedit earum deleniti sed saepe iure ea!Lorem Lorem, ipsum dolor
+            sit amet consectetur adipisicing elit. Adipisci ab culpa corrupti
+            modi, consequuntur fugit delectus quas rerum provident magni dolor a
+            excepturi repellendus nam quia! Doloribus Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Veritatis repudiandae aperiam
+            repellat, amet, molestiae ullam c odit libero sequi.umque, debitis
+            perferendis veniam tempora error corrupti non impedit earum deleniti
+            sed saepe iure ea!Lorem Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Adipisci ab culpa corrupti modi, consequuntur
+            fugit delectus quas rerum provident magni dolor a excepturi
+            repellendus nam quia! Doloribus
+          </Text>
+        </Pressable>
         <Image source={logoImg} style={{ width: 300, height: 300 }} />
         <Button
           title="Press"
