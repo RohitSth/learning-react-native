02c131d0ec4cf362956e404cf2b641c01257b9bc
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-05-14 12:49:44 +0545

    learned about Alert component

---
+++ app/(tabs)/explore2.tsx
@@ -0,0 +1,103 @@
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  ScrollView,
+  Button,
+  Pressable,
+  Modal,
+  StatusBar,
+  ActivityIndicator,
+} from "react-native";
+
+import { useState } from "react";
+
+const logoImg = require("../../assets/images/adaptive-icon.png");
+
+export default function Explore2() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [showStaturBar, setShowStatusBar] = useState(false);
+  const [showLoadingIndicator, setShowLoadingIndicator] = useState(false);
+
+  return (
+    <View style={{ flex: 1, backgroundColor: "skyblue", padding: 60 }}>
+      <ActivityIndicator />
+      <ActivityIndicator size="large" />
+      <ActivityIndicator size="large" color="midnightblue" />
+      <ActivityIndicator
+        size="large"
+        color="midnightblue"
+        animating={showLoadingIndicator}
+      />
+
+      <StatusBar
+        backgroundColor="pink"
+        barStyle="light-content"
+        hidden={showStaturBar}
+      />
+      <ScrollView>
+        <Button
+          title="StatusBar Visibility"
+          onPress={() => setShowStatusBar(!showStaturBar)}
+        />
+        <Button
+          title="Loading Indicator Visibility"
+          onPress={() => setShowLoadingIndicator(!showLoadingIndicator)}
+        />
+        <Pressable
+          onPressIn={() => console.log("Image Pressed In")}
+          onLongPress={() => console.log("Image Hold")}
+          onPressOut={() => console.log("Pressed Out")}
+        >
+          <Image
+            source={{ uri: "https://picsum.photos/seed/picsum/200/300" }}
+            style={{ width: 300, height: 300 }}
+          />
+        </Pressable>
+        <Pressable onPress={() => console.log("Text Pressed")}>
+          <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            repudiandae aperiam repellat, amet, molestiae ullam c odit libero
+            sequi.umque, debitis perferendis veniam tempora error corrupti non
+            impedit earum deleniti sed saepe iure ea!Lorem Lorem, ipsum dolor
+            sit amet consectetur adipisicing elit. Adipisci ab culpa corrupti
+            modi, consequuntur fugit delectus quas rerum provident magni dolor a
+            excepturi repellendus nam quia! Doloribus Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Veritatis repudiandae aperiam
+            repellat, amet, molestiae ullam c odit libero sequi.umque, debitis
+            perferendis veniam tempora error corrupti non impedit earum deleniti
+            sed saepe iure ea!Lorem Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Adipisci ab culpa corrupti modi, consequuntur
+            fugit delectus quas rerum provident magni dolor a excepturi
+            repellendus nam quia! Doloribus
+          </Text>
+        </Pressable>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        <Button
+          title="Press"
+          onPress={() => setModalVisible(true)}
+          color="midnightblue"
+          // disabled
+        />
+        <Modal
+          visible={modalVisible}
+          onRequestClose={() => setModalVisible(false)}
+          // animationType="fade"
+          animationType="slide"
+          // presentationStyle="formSheet" // iOS only
+          presentationStyle="pageSheet" // iOS only
+        >
+          <View style={{ flex: 1, backgroundColor: "blue", padding: 60 }}>
+            <Text>Modal Content</Text>
+            <Button
+              title="Close"
+              onPress={() => setModalVisible(false)}
+              color="midnightblue"
+            />
+          </View>
+        </Modal>
+      </ScrollView>
+    </View>
+  );
+}
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,103 +1,27 @@
-import {
-  View,
-  Text,
-  Image,
-  ImageBackground,
-  ScrollView,
-  Button,
-  Pressable,
-  Modal,
-  StatusBar,
-  ActivityIndicator,
-} from "react-native";
+import { View, Text, Alert, Button } from "react-native";
 
 import { useState } from "react";
 
 const logoImg = require("../../assets/images/adaptive-icon.png");
 
 export default function App() {
-  const [modalVisible, setModalVisible] = useState(false);
-  const [showStaturBar, setShowStatusBar] = useState(false);
-  const [showLoadingIndicator, setShowLoadingIndicator] = useState(false);
-
   return (
     <View style={{ flex: 1, backgroundColor: "skyblue", padding: 60 }}>
-      <ActivityIndicator />
-      <ActivityIndicator size="large" />
-      <ActivityIndicator size="large" color="midnightblue" />
-      <ActivityIndicator
-        size="large"
-        color="midnightblue"
-        animating={showLoadingIndicator}
+      <Button title="Alert" onPress={() => Alert.alert("Invalid data!")} />
+      <Button
+        title="Alert2"
+        onPress={() => Alert.alert("Invalid data!", "DOB Incorrect!")}
       />
-
-      <StatusBar
-        backgroundColor="pink"
-        barStyle="light-content"
-        hidden={showStaturBar}
+      <Button
+        title="Alert3"
+        onPress={() =>
+          Alert.alert("Invalid data!", "DOB Incorrect!", [
+            { text: "Cancel", onPress: () => console.log("Cancel Pressed") },
+            { text: "OK", onPress: () => console.log("OK Pressed") },
+          ])
+        }
       />
-      <ScrollView>
-        <Button
-          title="StatusBar Visibility"
-          onPress={() => setShowStatusBar(!showStaturBar)}
-        />
-        <Button
-          title="Loading Indicator Visibility"
-          onPress={() => setShowLoadingIndicator(!showLoadingIndicator)}
-        />
-        <Pressable
-          onPressIn={() => console.log("Image Pressed In")}
-          onLongPress={() => console.log("Image Hold")}
-          onPressOut={() => console.log("Pressed Out")}
-        >
-          <Image
-            source={{ uri: "https://picsum.photos/seed/picsum/200/300" }}
-            style={{ width: 300, height: 300 }}
-          />
-        </Pressable>
-        <Pressable onPress={() => console.log("Text Pressed")}>
-          <Text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-            repudiandae aperiam repellat, amet, molestiae ullam c odit libero
-            sequi.umque, debitis perferendis veniam tempora error corrupti non
-            impedit earum deleniti sed saepe iure ea!Lorem Lorem, ipsum dolor
-            sit amet consectetur adipisicing elit. Adipisci ab culpa corrupti
-            modi, consequuntur fugit delectus quas rerum provident magni dolor a
-            excepturi repellendus nam quia! Doloribus Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Veritatis repudiandae aperiam
-            repellat, amet, molestiae ullam c odit libero sequi.umque, debitis
-            perferendis veniam tempora error corrupti non impedit earum deleniti
-            sed saepe iure ea!Lorem Lorem, ipsum dolor sit amet consectetur
-            adipisicing elit. Adipisci ab culpa corrupti modi, consequuntur
-            fugit delectus quas rerum provident magni dolor a excepturi
-            repellendus nam quia! Doloribus
-          </Text>
-        </Pressable>
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-        <Button
-          title="Press"
-          onPress={() => setModalVisible(true)}
-          color="midnightblue"
-          // disabled
-        />
-        <Modal
-          visible={modalVisible}
-          onRequestClose={() => setModalVisible(false)}
-          // animationType="fade"
-          animationType="slide"
-          // presentationStyle="formSheet" // iOS only
-          presentationStyle="pageSheet" // iOS only
-        >
-          <View style={{ flex: 1, backgroundColor: "blue", padding: 60 }}>
-            <Text>Modal Content</Text>
-            <Button
-              title="Close"
-              onPress={() => setModalVisible(false)}
-              color="midnightblue"
-            />
-          </View>
-        </Modal>
-      </ScrollView>
+      <Text>Home</Text>
     </View>
   );
 }
