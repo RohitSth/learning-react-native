52363e3cae648cd2cec27a6af24532e572c8278b
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-05-15 08:21:17 +0545

    learned about box modal

--- app/(tabs)/styling.tsx
+++ app/(tabs)/styling.tsx
@@ -4,7 +4,15 @@ export default function Styling() {
   return (
     <View style={styles.container}>
       <View style={[styles.box, styles.greenBg]}>
-        <Text>Green Box</Text>
+        <Text
+          style={{
+            borderRadius: 10, // only applies in android not in ios
+            backgroundColor: "lightblue",
+            padding: 10,
+          }}
+        >
+          Green Box
+        </Text>
       </View>
       <View style={[styles.box, styles.grayBg]}>
         <Text>Black Box</Text>
@@ -21,9 +29,19 @@ const styles = StyleSheet.create({
     padding: 60,
   },
   box: {
-    width: 100,
-    height: 100,
-    padding: 10,
+    // box style
+    width: "25%",
+    height: "25%",
+    // padding: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    // margin: 10,
+    // marginHorizontal: 10,
+    marginVertical: 10,
+    borderWidth: 2,
+    borderColor: "black",
+    // borderStyle: "solid", // solid, dotted, dashed -- solid byy default
+    borderRadius: 10, // works in ios and android
   },
   title: {
     fontSize: 24,
