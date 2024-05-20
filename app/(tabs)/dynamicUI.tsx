488b6b8b768c991f676f057eecf86f79841ca2bb
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-05-20 08:50:48 +0545

    learned about dimensions API

--- app/(tabs)/dynamicUI.tsx
+++ app/(tabs)/dynamicUI.tsx
@@ -1,12 +1,17 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 
 export default function DynamicUI() {
   return (
     <View style={styles.container}>
-      <Text>Dynamic UI</Text>
+      <View style={styles.box}>
+        <Text style={styles.text}>Dynamic UI</Text>
+      </View>
     </View>
   );
 }
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   container: {
@@ -14,5 +19,17 @@ const styles = StyleSheet.create({
     backgroundColor: "lime",
     alignItems: "center",
     justifyContent: "center",
+  },
+  box: {
+    width: windowWidth > 500 ? "70%" : "90%",
+    height: windowHeight > 600 ? "60%" : "90%",
+    backgroundColor: "skyblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: windowWidth > 500 ? 50 : 24,
+    fontWeight: "bold",
   },
 });
