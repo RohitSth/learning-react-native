04074d7f3d906dc241c3ecec753b6dca9ce616a8
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-05-21 06:29:48 +0545

    learned about what are the drawbacks of dimensions api and also solved it to some point

--- app/(tabs)/dynamicUI.tsx
+++ app/(tabs)/dynamicUI.tsx
@@ -1,17 +1,43 @@
 import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { useState, useEffect } from "react";
 
 export default function DynamicUI() {
+  const [dimensions, setDimensions] = useState({
+    window: Dimensions.get("window"),
+  });
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener("change", ({ window }) => {
+      setDimensions({ window });
+    });
+    return () => subscription?.remove();
+  });
+
+  const { window } = dimensions;
+  const windowWidth = window.width;
+  const windowHeight = window.height;
+
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Dynamic UI</Text>
+      <View
+        style={[
+          styles.box,
+          {
+            width: windowWidth > 500 ? "70%" : "90%",
+            height: windowHeight > 600 ? "60%" : "90%",
+          },
+        ]}
+      >
+        <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>
+          Dynamic UI
+        </Text>
       </View>
     </View>
   );
 }
 
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
+// const windowWidth = Dimensions.get("window").width;
+// const windowHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   container: {
@@ -21,15 +47,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   box: {
-    width: windowWidth > 500 ? "70%" : "90%",
-    height: windowHeight > 600 ? "60%" : "90%",
+    // width: windowWidth > 500 ? "70%" : "90%",
+    // height: windowHeight > 600 ? "60%" : "90%",
     backgroundColor: "skyblue",
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    color: "white",
-    fontSize: windowWidth > 500 ? 50 : 24,
-    fontWeight: "bold",
-  },
+  // text: {
+  //   color: "white",
+  //   fontSize: windowWidth > 500 ? 50 : 24,
+  //   fontWeight: "bold",
+  // },
 });
